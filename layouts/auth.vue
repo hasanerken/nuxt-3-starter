b25c26e0fd339e3f2ce1cf0ffbca6301be05afbd
Hasan <hasan.erken@gmail.com>
2023-07-11 20:20:48 +0300

    Initial commit

---
+++ layouts/auth.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="page justify-center items-center">
+        <div class="p-4 w-full sm:max-w-xs">
+            <slot></slot>
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({ auth: false, middleware: "guest", colorMode: "light", layout: "public" });
+</script>
